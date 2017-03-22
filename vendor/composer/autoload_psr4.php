8a851130986c4c3a7efca9bcaa2489afbeccc618
Cristian Tala Sanchez <yomismo@cristiantala.cl>
2017-03-22 12:03:34 +0100

    HTTP RESPONSE, new AUTOLOAD

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ctala\\httphelper\\' => array($baseDir . '/'),
+    'ctala\\httphelper\\' => array($baseDir . '/classes'),
 );
